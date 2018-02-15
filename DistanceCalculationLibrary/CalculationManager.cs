92c0531d9cbddedaae02d80ef1529e8fed716c83
Vladislav Sarunov <sv415@greenwich.ac.uk>
2018-02-15 20:24:54 +0000

    Started Adding distance calculator

---
+++ DistanceCalculationLibrary/CalculationManager.cs
@@ -0,0 +1,16 @@
+﻿namespace DistanceCalculationLibrary
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public static class CalculationManager
+    {
+        public static void CalculateRoute(string startStation, string finishStations)
+        {
+
+        }
+    }
+}
---
+++ DistanceCalculationLibrary/DistanceCalculator.cs
@@ -0,0 +1,29 @@
+﻿namespace DistanceCalculationLibrary
+{
+    using DataAccess.Repository;
+    using Infrastructure.Entities;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class DistanceCalculator
+    {
+        private readonly IMainRepository MainRepo = new MainRepository();
+        private readonly List<DelayModel> Delays = new List<DelayModel>();
+        private readonly List<TrainLine> TrainLines = new List<TrainLine>();
+        private readonly List<Station> Stations = new List<Station>();
+        public DistanceCalculator()
+        {
+            this.Delays.AddRange(this.MainRepo.ReadDelay());
+            this.TrainLines.AddRange(this.MainRepo.ReadTrainLines());
+            this.Stations.AddRange(this.MainRepo.ReadStations());
+        }
+
+        public static void CalculateRoute(string startStation, string finishStations)
+        {
+
+        }
+    }
+}
