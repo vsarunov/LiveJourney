a75b7083cae815ef4cb68790b15a19d0a8b28bd3
Vladislav Sarunov <mrvo788@gmail.com>
2018-03-04 21:32:31 +0000

    Added Web client
    stated work on the client

---
+++ Client/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿namespace Client.Controllers
+{
+    using Client.Models;
+    using DataAccess.Repository;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+    using WebService;
+
+    public class HomeController : Controller
+    {
+        private readonly IMainRepository MainRepo = new MainRepository();
+
+        public ActionResult Index()
+        {
+            var stations = GetStations();
+
+            var model = new SearchViewModel()
+            {
+                Stations = stations
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(SearchViewModel postModel)
+        {
+            WebService1 myService = new WebService1();
+            var date = DateTime.Now.Date + new TimeSpan(14, 0, 0);
+            myService.GetDirections("Stanmore", "Finsbury Park", date);
+            return View();
+        }
+
+        private IEnumerable<SelectListItem> GetStations()
+        {
+            List<SelectListItem> ddl = new List<SelectListItem>();
+
+            var Stations = MainRepo.ReadStations().Select(x => x.StationName).Distinct();
+
+            foreach (var stationName in Stations)
+            {
+                ddl.Add(new SelectListItem { Text = stationName });
+            }
+
+            IEnumerable<SelectListItem> stationNames = ddl;
+
+            return stationNames;
+        }
+    }
+}
---
+++ Client/Models/SearchViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Client.Models
+{
+    public class SearchViewModel
+    {
+
+        [Required]
+        [Display(Name = "Start Station")]
+        public string StartStation { get; set; }
+
+        [Required]
+        [Display(Name = "Destination Station")]
+        public string DestinationStation { get; set; }
+
+        public IEnumerable<SelectListItem> Stations { get; set; }
+    }
+}
